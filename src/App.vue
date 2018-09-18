06aed9fe626c32a0fd725b13e5ac855ee48ba098
WuCY13 <drifterinnocent@gmail.com>
2018-09-18 10:46:37 +0800

    initiate project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <mySelect></mySelect>
+  </div>
+</template>
+
+<script>
+import mySelect from "@/components/mySelect"
+export default {
+  name: 'App',
+  components: {
+    mySelect,
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
