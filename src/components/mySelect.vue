84706f11ca9a449b690a9e29891e14c0ba9aa65d
WuCY13 <drifterinnocent@gmail.com>
2018-09-18 16:24:59 +0800

    adjust cascader

--- src/components/mySelect.vue
+++ src/components/mySelect.vue
@@ -10,7 +10,7 @@
           :value="item.value">
         </el-option>
       </el-select>
-      <el-select v-model="splitterValue" clearable :disabled=isSplitterDisabled placeholder="请选择Spliter">
+      <el-select v-model="splitterValue" clearable :disabled=isSplitterDisabled :placeholder=placeholderStatus>
         <el-option
           v-for="item in splitterList"
           :key="item.value"
@@ -18,7 +18,7 @@
           :value="item.value">
         </el-option>
       </el-select>
-      <el-select v-model="brandValue" clearable :disabled=isBrandDisabled placeholder="请选择Brand">
+      <el-select v-model="brandValue" clearable :disabled=isBrandDisabled :placeholder=placeholderStatus>
         <el-option
           v-for="item in brandlist"
           :key="item.value"
@@ -32,90 +32,101 @@
 
 <script>
   export default {
-  name: "myselect",
-  data() {
-  return {
-    isSplitterDisabled: true,
-    isBrandDisabled: true,
-    selectedOptions: [],
-    siteValue:'',
-    index1:'',
-    splitterValue:'',
-    index2:'',
-    brandValue:'',
-    index3:'',
-    splitterList:[],
-    brandlist:[],
-    siteList: [
-      {
-        url: "japan.com",
-        value: 0,
-        label: "japan",
-        children: [
+    name: "myselect",
+    data() {
+      return {
+        placeholderStatus:'请选择Site!',
+        isSplitterDisabled: true,
+        isBrandDisabled: true,
+        selectedOptions: [],
+        siteValue: null,
+        index1:'',
+        splitterValue:'',
+        index2:'',
+        brandValue:'',
+        index3:'',
+        splitterList:[],
+        brandlist:[],
+        siteList: [
           {
-            url: "jp.laptop.com",
-            value:0,
-            label: "laptop",
-            children: []
+            url: "japan.com",
+            value: 0,
+            label: "japan",
+            children: [
+              {
+                url: "jp.laptop.com",
+                value:0,
+                label: "laptop",
+                children: []
+              },
+              {
+                url: "jp.deskyop.com",
+                value:1,
+                label: "desktop",
+                children: []
+              }
+            ]
           },
           {
-            url: "jp.deskyop.com",
-            value:1,
-            label: "desktop",
-            children: []
+            url: "korea.com",
+            value: 1,
+            label: "korea",
+            children: [
+              {
+                url: "ko.laptop.com",
+                value:0,
+                label: "laptop",
+                children: []
+              },
+              {
+                url: "ko.desktop.com",
+                value:1,
+                label: "desktop",
+                children: []
+              }
+            ]
           }
-        ]
-      },
-      {
-        url: "korea.com",
-        value: 0,
-        label: "korea",
-        children: [
-          {
-            url: "ko.laptop.com",
-            value:0,
-            label: "laptop",
-            children: []
-          },
-          {
-            url: "ko.desktop.com",
-            value:1,
-            label: "desktop",
-            children: []
-          }
-        ]
+        ],
+        props: {
+          value: 'value',
+          label: 'label',
+          children: 'children'
+        },
+        serieslist: [],
+        subserieslist: []
       }
-    ],
-    props: {
-      value: 'value',
-      label: 'label',
-      children: 'children'
-    },
-    serieslist: [],
-    subserieslist: []
-  }
-  },
-  methods: {
-  handleChange(value) {
-    // console.log(value);
-  },
-  handleItemChange(val) {
-    console.log('active item:', val);
-    // console.log(this.siteList[0]);
-    this.siteList[0].children[0].children = [
-      {
-        value: "thinkpad.com",
-        label: "thinkpad",
       },
-    ]
-  },
-  changeSite(val){
-    //console.log(val)
-    this.isSplitterDisabled = false
-    this.splitterList = this.siteList[val].children
-    //console.log(this.splitterList)
-  },
-  }
+      watch: {
+        siteValue: function(){
+        (this.siteValue === null)?this.placeholderStatus='请选择Site!' :this.placeholderStatus='请选择'
+        }
+      },
+      computed: {
+        placeholderStatusSplitter: siteValue => {
+          return  (this.siteValue)?'请选择Spliter': '请选择Site!'
+        }
+      },
+      methods: {
+      handleChange(value) {
+        // console.log(value);
+      },
+      handleItemChange(val) {
+        console.log('active item:', val);
+        // console.log(this.siteList[0]);
+        this.siteList[0].children[0].children = [
+          {
+            value: "thinkpad.com",
+            label: "thinkpad",
+          },
+        ]
+      },
+      changeSite(val){
+        this.isSplitterDisabled = false;
+        this.isBrandDisabled = false;
+        this.splitterList = this.siteList[val].children
+        this.brandList = this.siteList[val].children
+      },
+    }
   }
 </script>
 
