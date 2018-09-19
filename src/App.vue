d9af2096977b3545137b734c96d7af2229434cc3
WuCY13 <drifterinnocent@gmail.com>
2018-09-19 10:49:27 +0800

    add new component submitButton

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <el-container><!--elementUi 外层容器-->
       <el-main><!--elementUi 外层容器-->
         <mySelect></mySelect><!--选择菜单-->
+        <submitButton></submitButton>
         <resultList></resultList><!--结果列表-->
       </el-main>
     </el-container>
@@ -12,11 +13,13 @@
 <script>
 import mySelect from "@/components/mySelect"
 import resultList from '@/components/resultList'
+import submitButton from '@/components/submitButton'
 export default {
   name: 'App',
   components: {
     mySelect,
     resultList,
+    submitButton,
   },
 }
 </script>
---
+++ src/components/submitButton.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div id="submitButton">
+    <el-button type="primary" icon="el-icon-search" circle></el-button>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+  #submitButton {
+    margin:auto;
+    text-align:center;
+    margin-bottom: 10px;
+  }
+</style>
