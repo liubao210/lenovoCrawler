a62e977d59981d4bcc7bbd467b4ea8a4a307fa03
WuCY13 <drifterinnocent@gmail.com>
2018-09-21 16:32:00 +0800

    add component tabControlSelect

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,10 @@
       <el-main><!--elementUi 外层容器-->
         <mySelect></mySelect><!--选择菜单-->
         <submitButton></submitButton>
+        <tabControlSelect></tabControlSelect>
         <div id="resultHolder">
           <tabControl></tabControl><!--页面切换-->
         </div>
-
-
       </el-main>
     </el-container>
   </div>
@@ -16,16 +15,16 @@
 
 <script>
 import mySelect from "@/components/mySelect"
-//import resultList from '@/components/resultList'
 import submitButton from '@/components/submitButton'
 import tabControl from '@/components/tabControl'
+import tabControlSelect from '@/components/tabControlSelect'
 export default {
   name: 'App',
   components: {
     mySelect,
-    //resultList,
     submitButton,
     tabControl,
+    tabControlSelect,
   },
 }
 </script>
---
+++ src/components/tabControlSelect.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="tabControlSelect">
+    <el-tabs :tab-position="tabPosition" style="height: 200px;">
+      <el-tab-pane :label="splitterLable">用户管理</el-tab-pane>
+      <el-tab-pane label="配置管理">配置管理</el-tab-pane>
+      <el-tab-pane label="角色管理">角色管理</el-tab-pane>
+      <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        tabPosition: 'left',
+        splitterLable: 'Desktop',
+      };
+    }
+  };
+</script>
+
+<style scoped>
+  #tabControlSelect {
+    margin: auto;
+    margin-top: 30px;
+    margin-bottom: 30px;
+    background-color: #EBEEF5;
+    padding: 5px;
+    border-radius: 10px;
+
+  }
+</style>
