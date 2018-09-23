cf530ffa13348a15f0f034493e0c276f08e4abb1
sub/dance <957839431@qq.com>
2018-09-23 22:54:41 +0800

    adjust on loop on loop

--- src/components/tabControlSelect.vue
+++ src/components/tabControlSelect.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="tabControlSelect">
-    <el-tabs :tab-position="tabPosition" style="height: 200px;">
+    <el-tabs tab-position=left style="height: 200px;">
       <el-tab-pane
-        v-for='(splitter, index) in splitterLables'
-        :label='splitter.splitterName'>
-        用户管理
+        v-for='(splitter) in splitterLables'
+        :label='splitter.splitterNames'>
+        <span
+          v-for='(brand, index) in splitter.brandNames'>
+          {{ brand }}
+        </span>
       </el-tab-pane>
-      <el-tab-pane label="配置  管理">配置管理</el-tab-pane>
-      <el-tab-pane label="角色管理">角色管理</el-tab-pane>
-      <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
     </el-tabs>
   </div>
 </template>
@@ -16,11 +16,23 @@
   export default {
     data() {
       return {
+        selectedIndex: 0,
         tabPosition: 'left',
         splitterLables: [
           {
-            splitterName:'Desktop',
-
+            splitterNames:'Desktop',
+            brandNames: [
+              'ThinKPad',
+              'Yoga',
+              'IdeaPad',
+            ],
+          },
+          {
+            splitterNames: 'WorkStation',
+            brandNames: [
+              'ThinkStation',
+              'ThinkServer',
+            ],
           },
 
         ],
